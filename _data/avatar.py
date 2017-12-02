8ace10d3a70e36e5866ae524870db79f23cd8afa
German Martinez <germaaan@gmail.com>
2017-12-02 16:16:34 +0100

    Añade scraping avatar GitHub

---
+++ _data/avatar.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding=utf8
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+'''
+Dependencies:
+    PyYAML ^3.12
+'''
+
+import yaml
+import urllib2
+import json
+
+token = open("access_token","r").read()
+authors = {}
+avatars = {}
+
+with open("authors.yml", "r") as f:
+    authors = yaml.load(f)
+
+    for key in authors:
+        user = json.loads(urllib2.urlopen("https://api.github.com/users/" + key + "?access_token=" + token).read())
+        avatars[key] = user["avatar_url"]
+
+for key, value in avatars.items():
+    authors[key]["avatar"] = value
+
+for key, value in authors.items():
+    print (key, value)
+
+with open('authors.yml', 'w') as f:
+    yaml.safe_dump(authors, f, default_flow_style = False, allow_unicode = True)
